54023e89318d9b5cf77071d8d0711c00f8d8c6e7
charlesncn <charowcjn@gmail.com>
2023-07-03 15:52:22 +0300

    Send message

--- src/main/java/com/cndev/KafkaBrokerApplication.java
+++ src/main/java/com/cndev/KafkaBrokerApplication.java
@@ -1,13 +1,21 @@
 package com.cndev;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaBrokerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaBrokerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return  args -> kafkaTemplate.send("dev", "Testing kafka");
     }
 
 }
