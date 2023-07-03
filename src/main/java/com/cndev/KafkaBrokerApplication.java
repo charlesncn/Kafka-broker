eef811097541a92f040dc866b2da69bca8610161
charlesncn <charowcjn@gmail.com>
2023-07-03 15:34:57 +0300

    Feature: Topic setup

---
+++ src/main/java/com/cndev/KafkaBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.cndev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBrokerApplication.class, args);
+    }
+
+}
